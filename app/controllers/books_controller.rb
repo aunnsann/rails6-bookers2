7f28f995c90448f3ff85a502bda7c934aa23041c
aunnsann <127009562+aunnsann@users.noreply.github.com>
2023-06-04 09:42:20 +0000

    応用課題２

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+
+  def show
+    @user = current_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+    @users = User.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @user = current_user
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
